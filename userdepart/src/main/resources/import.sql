ff57b13b13e1149f4a474e2e801c52e273cdd7ec
LucaszLisboa <gabriel.lisboa.141@gmail.com>
2023-01-04 17:49:37 -0300

    :electron: h2 database configuration

---
+++ userdepart/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('TI');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Maria Fernanda', 'maria@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Carlos Souza', 'bob@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Alex Moraes', 'alex@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Lucas Lisboa', 'lucas@gmail.com');
